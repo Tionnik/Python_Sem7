2a628e9ef90c5ce58d0af75e747ad9b16fc2048d
Nik <ice_fire50@mail.ru>
2023-05-22 13:45:12 +0300

    homework sem7.1

--- task_36.py
+++ task_36.py
@@ -8,7 +8,7 @@ def print_operation_table(operation, num_rows=6, num_columns=6):
     string= []
     for j in range(1, num_rows+1):
         for i in range(1, num_columns+1):
-            string.append(operation(i,j))
+            string.append(operation(j,i))
         print(string)
         string= []  
 
